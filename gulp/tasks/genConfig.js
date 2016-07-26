64acc1fa57955c1e303e87f8d2a1718d39843a81
David Carter <dcarter@freewheel.tv>
2016-07-26 13:54:00 -0400

    rewrite genConfig to generate backstop.json from JS script

--- gulp/tasks/genConfig.js
+++ gulp/tasks/genConfig.js
@@ -5,8 +5,23 @@ var argv   = require('yargs').argv;
 /**
  * Copies a boilerplate config file to the current config file location.
  */
-gulp.task('genConfig', ['genScripts'], function(){
-  return gulp.src(paths.captureConfigFileNameDefault)
-    .pipe(rename(paths.backstopConfigFileName))
-    .pipe(gulp.dest('/'));
+// gulp.task('genConfig', ['genScripts'], function(){
+//   return gulp.src(paths.captureConfigFileNameDefault)
+//     .pipe(rename(paths.backstopConfigFileName))
+//     .pipe(gulp.dest('/'));
+// });
+
+gulp.task('genConfig', function() {
+  if(fsx.existsSync(paths.backstopJSConfigPath)) {  
+    var JSconfig = require(paths.backstopJSConfigPath);
+    var oldConfigJson = fsx.readJsonSync(paths.activeCaptureConfigPath, { throws: false }) || {};
+    var newChecksum = checksum(JSON.stringify(JSconfig));
+
+    if(newChecksum !== oldConfigJson.checksum) {
+      console.log("Detected changes in JS generator script, writing new backstop.json config file..");
+      var newConfig = require(paths.backstopJSConfigPath);
+      newConfig.checksum = newChecksum;
+      fsx.outputJsonSync(paths.activeCaptureConfigPath, newConfig);
+    }
+  }
 });
